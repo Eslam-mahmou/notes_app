ba4d5c35a04d7b5d5860362e55de934b5ed1d741
Eslam_Mahmoud <solom6112002@gmail.com>
2023-10-01 16:20:14 +0300

    CustomListTile

--- lib/components/CustomAppBar.dart
+++ lib/components/CustomAppBar.dart
@@ -1,11 +1,43 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
+  const CustomAppBar({super.key,required this.text,required this.icon});
+final String text;
+final Icon icon;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(text,
+        style: TextStyle(
+          fontSize: 25
+        ),),
+       
+        CustomIcon(
+          icon:icon ,
+        ),
+      ],
+    );
+  }
+}
+
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key,required this.icon});
+final Icon icon;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color:const Color(0xff606060).withOpacity(.2)
+      ),
+      height: 45,
+      width: 45,
+      child: IconButton(onPressed: (){
+
+      }, icon: icon,
+      color: Colors.white.withOpacity(.5),),
+    );
   }
 }
--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_test/components/CustomAppBar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,29 +7,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xff303030),
-        elevation: 0,
-        title: Row(
-          children: [
-          
-           const Text('Notes',
-            style: TextStyle(
-              fontSize: 26
-            ),),
-          const Spacer(),
-           Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Color(0xff303030).withOpacity(.05)
-            ),
-            height: 45,
-            width: 45,
-           child: IconButton(onPressed: (){}, icon: Icon(Icons.search,
-             color: Colors.white.withOpacity(.6),)),
-           )
-          ],
-        ),
+      body: Column(
+        children:const  [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(
+            text: 'Notes',
+            icon: Icon(Icons.search),
+          ),
+        ],
       ),
     );
   }
